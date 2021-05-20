29bd95361e321c26fd01a241274138609f95b373
IgorOliveira <igordeeoliveira@gmail.com>
2021-05-20 19:14:17 -0300

    Update formToGTM.js

--- formToGTM.js
+++ formToGTM.js
@@ -3,23 +3,25 @@ document.addEventListener("DOMContentLoaded", function() {
     const NUMBER_EMPLOYEES_FIELD = 111153;
 
     const form = document.querySelector('form[action^="https://www.rdstation.com.br/api/"]');
-    form.addEventListener('submit', (event) => {
+    if(form){
+      form.addEventListener('submit', (event) => {
+        
+          const formData = Object.fromEntries(new FormData(form).entries());
+          const numberOfEmployees = formData[`custom_fields[${NUMBER_EMPLOYEES_FIELD}]`];
 
-        const formData = Object.fromEntries(new FormData(form).entries());
-        const numberOfEmployees = formData[`custom_fields[${NUMBER_EMPLOYEES_FIELD}]`];
+          if(numberOfEmployees === ""){
+              return;
+          }
 
-        if(numberOfEmployees === ""){
-            return;
-        }
+          if(!window.dataLayer){
+              console.error('GTM DATALAYER GTM NÃO EXISTE NA PÁGINA');
+          }
 
-        if(!window.dataLayer){
-            console.error('GTM DATALAYER GTM NÃO EXISTE NA PÁGINA');
-        }
-
-        window.dataLayer.push({
-            'event': 'form-confirmation',
-            '100+_funcionarios': ['Mais de 1000', 'De 501 a 1000', 'De 201 a 500', 'De 101 a 200'].includes(numberOfEmployees)
-        })
-    })
+          window.dataLayer.push({
+              'event': 'form-confirmation',
+              '100+_funcionarios': ['Mais de 1000', 'De 501 a 1000', 'De 201 a 500', 'De 101 a 200'].includes(numberOfEmployees)
+          })
+      })
+    }
 });
 
