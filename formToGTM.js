09bda2d96a10301bc43e0138b99599e73c80bd3f
IgorOliveira <igordeeoliveira@gmail.com>
2021-05-20 18:27:44 -0300

    Update formToGTM.js

--- formToGTM.js
+++ formToGTM.js
@@ -1,23 +1,25 @@
-// ID DO CAMPO "NUMERO DE FUNCIONARIOS"
-const NUMBER_EMPLOYEES_FIELD = 111153
+document.addEventListener("DOMContentLoaded", function() {
+  // ID DO CAMPO "NUMERO DE FUNCIONARIOS"
+    const NUMBER_EMPLOYEES_FIELD = 111153;
 
-const form = document.querySelector('form[action^="https://www.rdstation.com.br/api/"]')
-form.addEventListener('submit', (event) => {
+    const form = document.querySelector('form[action^="https://www.rdstation.com.br/api/"]');
+    form.addEventListener('submit', (event) => {
 
-    const formData = Object.fromEntries(new FormData(form).entries());
-    const numberOfEmployees = formData[`custom_fields[${NUMBER_EMPLOYEES_FIELD}]`]
+        const formData = Object.fromEntries(new FormData(form).entries());
+        const numberOfEmployees = formData[`custom_fields[${NUMBER_EMPLOYEES_FIELD}]`];
 
-    if(numberOfEmployees === ""){
-        return;
-    }
-    
-    if(!window.dataLayer){
-        console.error('GTM DATALAYER GTM NÃO EXISTE NA PÁGINA')
-    }
+        if(numberOfEmployees === ""){
+            return;
+        }
 
-    console.log('tem mais de 100:',['Mais de 1000', 'De 501 a 1000', 'De 201 a 500', 'De 101 a 200'].includes(numberOfEmployees))
-    window.dataLayer.push({
-        'event': 'form-confirmation',
-        '100+_funcionarios': ['Mais de 1000', 'De 501 a 1000', 'De 201 a 500', 'De 101 a 200'].includes(numberOfEmployees)
+        if(!window.dataLayer){
+            console.error('GTM DATALAYER GTM NÃO EXISTE NA PÁGINA');
+        }
+
+        window.dataLayer.push({
+            'event': 'form-confirmation',
+            '100+_funcionarios': ['Mais de 1000', 'De 501 a 1000', 'De 201 a 500', 'De 101 a 200'].includes(numberOfEmployees);
+        })
     })
-})
+});
+
