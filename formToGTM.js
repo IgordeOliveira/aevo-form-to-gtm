810652433cf3f97970372bf7e759af7075ce83fe
IgorOliveira <igordeeoliveira@gmail.com>
2021-05-21 17:45:30 -0300

    Update formToGTM.js

--- formToGTM.js
+++ formToGTM.js
@@ -1,30 +1,31 @@
 'use strict';
 
-setInterval(function () {
+var formToGtmSearch = setInterval(function () {
     var form = document.querySelector('form[action^="https://www.rdstation.com.br/api/"]');
     if (form) {
-        (function () {
-            // ID DO CAMPO "NUMERO DE FUNCIONARIOS"
-            var NUMBER_EMPLOYEES_FIELD = 111153;
+        clearInterval(formToGtmSearch);
+        // ID DO CAMPO "NUMERO DE FUNCIONARIOS"
+        var NUMBER_EMPLOYEES_FIELD = 111153;
 
-            form.addEventListener('submit', function (event) {
+        form.addEventListener('submit', function (event) {
 
-                var formData = Object.fromEntries(new FormData(form).entries());
-                var numberOfEmployees = formData['custom_fields[' + NUMBER_EMPLOYEES_FIELD + ']'];
+            var formData = Object.fromEntries(new FormData(form).entries());
+            var numberOfEmployees = formData['custom_fields[' + NUMBER_EMPLOYEES_FIELD + ']'];
 
-                if (numberOfEmployees === "") {
-                    return;
-                }
+            if (numberOfEmployees === "") {
+                return;
+            }
 
-                if (!window.dataLayer) {
-                    console.error('GTM DATALAYER GTM NÃO EXISTE NA PÁGINA');
-                }
+            if (!window.dataLayer) {
+                console.error('GTM DATALAYER GTM NÃO EXISTE NA PÁGINA');
+            }
 
-                window.dataLayer.push({
-                    'event': 'form-confirmation',
-                    '100+_funcionarios': ['Mais de 1000', 'De 501 a 1000', 'De 201 a 500', 'De 101 a 200'].includes(numberOfEmployees)
-                });
+            window.dataLayer.push({
+                'event': 'form-confirmation',
+                '100+_funcionarios': ['Mais de 1000', 'De 501 a 1000', 'De 201 a 500', 'De 101 a 200'].includes(numberOfEmployees)
             });
-        })();
+        });
     }
-}, 200);
+}, 500);
+
+setTimeout();
